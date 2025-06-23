607f10ad2286c151e5b997d3db84fd02b01ef915
Paul Amoah <51966806+paglobal@users.noreply.github.com>
2025-06-23 18:41:01 +0000

    Updated index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import { render } from "lit";
 import App from "./App";
 import "./index.css";
+import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path.js";
+
+setBasePath("/path/to/shoelace/dist");
 
 render(<App />, document.body);
