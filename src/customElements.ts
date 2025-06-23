21729b4d1b3743d1a03631d7ae969918978bd2e2
Paul Amoah <51966806+paglobal@users.noreply.github.com>
2025-06-23 18:58:08 +0000

    Updated customElements.ts

--- src/customElements.ts
+++ src/customElements.ts
@@ -1,2 +1,3 @@
 import "@shoelace-style/shoelace/dist/components/alert/alert.js";
 import "@shoelace-style/shoelace/dist/components/icon/icon.js";
+import "@shoelace-style/shoelace/dist/components/split-panel/split-panel.js";
