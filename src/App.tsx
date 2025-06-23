0211701c4e69432f692bbbf3a4c68d4bf208f5bc
Paul Amoah <51966806+paglobal@users.noreply.github.com>
2025-06-23 18:39:53 +0000

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,12 @@
-import { styleMap } from "lit/directives/style-map.js";
-import { Cell, generateMaze } from "./generateMaze";
+import "@shoelace-style/shoelace/dist/components/alert/alert.js";
+import "@shoelace-style/shoelace/dist/components/icon/icon.js";
 
 function App() {
-  const containerStyles = {
-    width: "100vw",
-    height: "100vh",
-    display: "flex",
-    justifyContent: "center",
-    flexDirection: "column",
-  };
-
-  const border = "1px solid black";
-  const cellSize = "60px";
-  const gridSize = 15;
-
-  const cellStyles = (cell: Cell) => ({
-    borderTop: cell.walls.top ? border : null,
-    borderLeft: cell.walls.left ? border : null,
-    borderBottom: cell.walls.bottom ? border : null,
-    borderRight: cell.walls.right ? border : null,
-    backgroundColor: "#08759E",
-    width: cellSize,
-    height: cellSize,
-  });
-
-  const rowStyles = {
-    display: "flex",
-  };
-
-  const maze = generateMaze(gridSize, gridSize);
-
   return () => (
-    <div $attr:style={styleMap(containerStyles)}>
-      {maze.map((row) => (
-        <div $attr:style={styleMap(rowStyles)}>
-          {row.map((cell) => (
-            <div $attr:style={styleMap(cellStyles(cell))}></div>
-          ))}
-        </div>
-      ))}
-    </div>
+    <sl-alert bool:open>
+      <sl-icon slot="icon" name="info-circle"></sl-icon>
+      This is a standard alert. You can customize its content and even the icon.
+    </sl-alert>
   );
 }
 
