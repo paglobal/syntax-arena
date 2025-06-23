158d732d0946968c744f78448c769bc5d2a589c4
Paul Amoah <51966806+paglobal@users.noreply.github.com>
2025-06-23 18:57:43 +0000

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 function App() {
   return () => (
-    <sl-alert open>
-      <sl-ico slot="icon" name="info-circle"></sl-ico>
-      This is a standard alert. You can customize its content and even the icon.
-    </sl-alert>
+    <sl-split-panel>
+      <div
+        slot="start"
+        style="height: 200px; background: var(--sl-color-neutral-50); display: flex; align-items: center; justify-content: center; overflow: hidden;"
+      >
+        Start
+      </div>
+      <div
+        slot="end"
+        style="height: 200px; background: var(--sl-color-neutral-50); display: flex; align-items: center; justify-content: center; overflow: hidden;"
+      >
+        End
+      </div>
+    </sl-split-panel>
   );
 }
 
