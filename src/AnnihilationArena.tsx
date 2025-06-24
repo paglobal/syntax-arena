3fbe44eb8e7d205b1985e425ed84ab37c75a9a37
Paul Amoah <51966806+paglobal@users.noreply.github.com>
2025-06-24 16:08:37 +0000

    Updated AnnihilationArena.tsx

--- src/AnnihilationArena.tsx
+++ src/AnnihilationArena.tsx
@@ -1,31 +1,12 @@
 import { createRef, ref } from "lit/directives/ref.js";
 import { adaptEffect } from "promethium-js";
-import { Application, Container } from "pixi.js";
+import { initializeGame } from "./gameLoop";
 
 export function AnnihilationArena() {
   const canvasRef = createRef<HTMLCanvasElement>();
 
   adaptEffect(() => {
-    async function init() {
-      const app = new Application();
-      await app.init({
-        background: "#fff",
-        canvas: canvasRef.value,
-        width: 600,
-        height: 600,
-      });
-
-      const container = new Container();
-      app.stage.addChild(container);
-      container.x = app.screen.width / 2;
-      container.y = app.screen.height / 2;
-
-      app.ticker.add((time) => {
-        container.rotation -= 0.01 * time.deltaTime;
-      });
-    }
-
-    init();
+    initializeGame(canvasRef.value);
   });
 
   return () => <canvas use:ref={ref(canvasRef)}></canvas>;
