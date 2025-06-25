76bccd619703ecc1020962a2db7e6dcd79c64b2e
Paul Amoah <51966806+paglobal@users.noreply.github.com>
2025-06-25 20:02:19 +0000

    Updated player.ts

---
+++ src/player.ts
@@ -0,0 +1,34 @@
+import { Assets, Container, RenderLayer, Sprite } from "pixi.js";
+import { adaptState, adaptSyncEffect } from "promethium-js";
+import { ARENA_CELL_SIZE, assetAliases, MID_POINT } from "./constants";
+
+const [playerState, setPlayerState] = adaptState({
+  health: 100,
+  position: { x: 1, y: 9 },
+});
+
+export function drawPlayerGraphics(stage: Container) {
+  const playerLayer = new RenderLayer();
+  const playerSprite = new Sprite({
+    texture: Assets.get(assetAliases.characters.player),
+    anchor: MID_POINT,
+    pivot: MID_POINT,
+  });
+  playerLayer.attach(playerSprite);
+
+  stage.addChild(playerLayer);
+  stage.addChild(playerSprite);
+
+  adaptSyncEffect(() => {
+    const _playerState = playerState();
+    const ratio = playerSprite.height / playerSprite.width;
+    playerSprite.position.set(
+      ARENA_CELL_SIZE * (_playerState.position.x + MID_POINT),
+      ARENA_CELL_SIZE * (_playerState.position.y + MID_POINT),
+    );
+    playerSprite.setSize(ARENA_CELL_SIZE, ARENA_CELL_SIZE * ratio);
+    playerSprite.angle = 360;
+  });
+}
+
+function updatePlayerState() {}
