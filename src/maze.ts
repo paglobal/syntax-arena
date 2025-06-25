2ec249b7ed1ea42f4224581eab358c741e916d29
Paul Amoah <51966806+paglobal@users.noreply.github.com>
2025-06-25 15:08:38 +0000

    Updated maze.ts

--- src/maze.ts
+++ src/maze.ts
@@ -1,14 +1,27 @@
-export interface Cell {
+import { Graphics, RenderLayer } from "pixi.js";
+import { adaptState, adaptSyncEffect } from "promethium-js";
+import {
+  ARENA_CELL_SIZE,
+  ARENA_COLUMN_COUNT,
+  ARENA_ROW_COUNT,
+  ARENA_WALL_THICKNESS,
+} from "./constants";
+
+type WallKind = "solid" | "breakable" | null;
+
+type Cell = {
   row: number;
   column: number;
   walls: {
-    top: boolean;
-    right: boolean;
-    bottom: boolean;
-    left: boolean;
+    top: WallKind;
+    right: WallKind;
+    bottom: WallKind;
+    left: WallKind;
   };
   visited: boolean;
-}
+};
+
+type Maze = Cell[][];
 
 const walls = { t: "top", r: "right", b: "bottom", l: "left" } as const;
 const neighborWalls = {
@@ -40,8 +53,10 @@ function getDirection(wallLetter: keyof typeof walls) {
 
 function createShuffledDirections(array: typeof directions): typeof directions {
   const shuffledDirections = [...array];
+
   for (let i = shuffledDirections.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
+
     [shuffledDirections[i], shuffledDirections[j]] = [
       shuffledDirections[j],
       shuffledDirections[i],
@@ -52,18 +67,20 @@ function createShuffledDirections(array: typeof directions): typeof directions {
 }
 
 export function generateMaze(noOfRows: number, noOfColumns: number) {
-  const maze: Cell[][] = [];
+  const maze: Maze = [];
+
   for (let row = 0; row < noOfRows; row++) {
     maze[row] = [];
+
     for (let column = 0; column < noOfColumns; column++) {
       maze[row][column] = {
         row,
         column,
         walls: {
-          top: true,
-          right: true,
-          bottom: true,
-          left: true,
+          top: "solid",
+          right: "solid",
+          bottom: "solid",
+          left: "solid",
         },
         visited: false,
       };
@@ -72,7 +89,9 @@ export function generateMaze(noOfRows: number, noOfColumns: number) {
 
   function dfs(row: number, column: number) {
     maze[row][column].visited = true;
+
     const shuffledDirections = createShuffledDirections(directions);
+
     for (const direction of shuffledDirections) {
       const newRow = row + direction.dr;
       const newColumn = column + direction.dc;
@@ -83,8 +102,8 @@ export function generateMaze(noOfRows: number, noOfColumns: number) {
         newColumn < noOfColumns;
 
       if (newRowIsValid && !maze[newRow][newColumn].visited) {
-        maze[row][column].walls[direction.wall] = false;
-        maze[newRow][newColumn].walls[direction.neighborWall] = false;
+        maze[row][column].walls[direction.wall] = null;
+        maze[newRow][newColumn].walls[direction.neighborWall] = null;
 
         dfs(newRow, newColumn);
       }
@@ -93,7 +112,55 @@ export function generateMaze(noOfRows: number, noOfColumns: number) {
 
   const startRow = 0;
   const startColumn = 0;
+
   dfs(startRow, startColumn);
 
   return maze;
 }
+
+const [mazeState, setMazeState] = adaptState(() =>
+  generateMaze(ARENA_ROW_COUNT, ARENA_COLUMN_COUNT),
+);
+
+export const mazeLayer = new RenderLayer();
+export const mazeGraphics = new Graphics();
+mazeLayer.attach(mazeGraphics);
+
+function drawMazeGraphics() {
+  const maze = mazeState();
+  adaptSyncEffect(() => {
+    mazeGraphics.clear();
+    for (let row = 0; row < maze.length; row++) {
+      for (let column = 0; column < maze[row].length; column++) {
+        const cell = maze[row][column];
+        const x = column * ARENA_CELL_SIZE;
+        const y = row * ARENA_CELL_SIZE;
+
+        if (cell.walls.top) {
+          mazeGraphics.moveTo(x, y);
+          mazeGraphics.lineTo(x + ARENA_CELL_SIZE, y);
+          // cell.walls.top === "breakable"
+          //   ? mazeGraphics.stroke({ width: 2, color: 0x00ffff })
+          //   : mazeGraphics.stroke({ width: 2, color: 0xffffff });
+        }
+        if (cell.walls.right) {
+          mazeGraphics.moveTo(x + ARENA_CELL_SIZE, y);
+          mazeGraphics.lineTo(x + ARENA_CELL_SIZE, y + ARENA_CELL_SIZE);
+        }
+        if (cell.walls.bottom) {
+          mazeGraphics.moveTo(x, y + ARENA_CELL_SIZE);
+          mazeGraphics.lineTo(x + ARENA_CELL_SIZE, y + ARENA_CELL_SIZE);
+        }
+        if (cell.walls.left) {
+          mazeGraphics.moveTo(x, y);
+          mazeGraphics.lineTo(x, y + ARENA_CELL_SIZE);
+        }
+      }
+      mazeGraphics.stroke({ width: ARENA_WALL_THICKNESS, color: 0xffffff });
+    }
+  });
+}
+
+drawMazeGraphics();
+
+function updateMazeState() {}
