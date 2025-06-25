405edf5a308e599c197cd3ac6d8298c8b2173ea5
Paul Amoah <51966806+paglobal@users.noreply.github.com>
2025-06-25 20:09:37 +0000

    Updated constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,25 @@
-export const ARENA_WIDTH = 600;
-export const ARENA_HEIGHT = 800;
-export const ARENA_CELL_SIZE = 40;
+export const ARENA_CELL_SIZE = 80;
+export const ARENA_ROW_COUNT = 10;
+export const ARENA_COLUMN_COUNT = 8;
+export const ARENA_WIDTH = ARENA_COLUMN_COUNT * ARENA_CELL_SIZE;
+export const ARENA_HEIGHT = ARENA_ROW_COUNT * ARENA_CELL_SIZE;
 export const ARENA_WALL_THICKNESS = 5;
-export const ARENA_ROW_COUNT = ARENA_HEIGHT / ARENA_CELL_SIZE;
-export const ARENA_COLUMN_COUNT = ARENA_WIDTH / ARENA_CELL_SIZE;
+export const MID_POINT = 0.5;
+
+export const assetFolders = {
+  characters: "characters",
+  objects: "objects",
+} as const;
+
+export const assetAliases = {
+  [assetFolders.characters]: {
+    player: "player",
+    blueEnemy: "blueEnemy",
+    redEnemy: "redEnemy",
+    orangeEnemy: "orangeEnemy",
+  },
+  [assetFolders.objects]: {
+    powerUp: "powerUp",
+    key: "key",
+  },
+} as const;
