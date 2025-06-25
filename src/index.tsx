4ce432730a153c52aaa662516302a84b13e8420d
Paul Amoah <51966806+paglobal@users.noreply.github.com>
2025-06-25 20:01:37 +0000

    Updated index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,27 @@ import { render } from "lit";
 import App from "./App";
 import "./index.css";
 import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path.js";
-import "./customElements.ts";
+import "./customElements";
+import { Assets } from "pixi.js";
+import { assetAliases, assetFolders } from "./constants";
 
 setBasePath("/");
+
+type AssetFolder = (typeof assetFolders)[keyof typeof assetFolders];
+
+function getSVGAssetObject(folder: AssetFolder, alias: string) {
+  return { alias, src: `/assets/${folder}/${alias}.svg` };
+}
+
+async function loadAssetBundle(folder: AssetFolder) {
+  const assetArray = Object.values(assetAliases[folder]).map((alias) =>
+    getSVGAssetObject(folder, alias),
+  );
+  Assets.addBundle(folder, assetArray);
+  await Assets.loadBundle(folder);
+}
+
+await loadAssetBundle(assetFolders.characters);
+await loadAssetBundle(assetFolders.objects);
 
 render(<App />, document.body);
