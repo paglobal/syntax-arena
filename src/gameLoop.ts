2bb47bc954a1ef1a0ac924d3144032452fccf84c
Paul Amoah <51966806+paglobal@users.noreply.github.com>
2025-06-26 16:13:27 +0000

    Updated gameLoop.ts

--- src/gameLoop.ts
+++ src/gameLoop.ts
@@ -3,15 +3,19 @@ import { Application } from "pixi.js";
 import { ARENA_HEIGHT, ARENA_WIDTH } from "./constants";
 import { adaptMemo, adaptState } from "promethium-js";
 import { drawPlayerGraphics } from "./player";
+import { getCSSVariable } from "./renderUtils";
+import { drawEnemiesGraphics } from "./enemies";
+import { drawKeysGraphics } from "./keys";
+import { drawPowerUpsGraphics } from "./powerUps";
 
-const [gameState, setGameState] = adaptState({ level: 1 });
-const derivedGameState = adaptMemo(() => gameState());
+const [gameControlState, setGameControlState] = adaptState({ level: 1 });
+const derivedGameState = adaptMemo(() => ({ state: gameControlState() }));
 
 export async function initializeGame(canvas?: HTMLCanvasElement) {
   const app = new Application();
   await app.init({
     autoStart: true,
-    background: "#000000",
+    background: getCSSVariable("--sl-color-neutral-0"),
     canvas,
     width: ARENA_WIDTH,
     height: ARENA_HEIGHT,
@@ -20,6 +24,9 @@ export async function initializeGame(canvas?: HTMLCanvasElement) {
   app.stage.addChild(mazeLayer);
   app.stage.addChild(mazeGraphics);
 
+  drawPowerUpsGraphics(app.stage);
+  drawKeysGraphics(app.stage);
+  drawEnemiesGraphics(app.stage);
   drawPlayerGraphics(app.stage);
 
   app.ticker.add((ticker) => {
