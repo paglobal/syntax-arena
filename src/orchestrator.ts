c015a611c613e9543e8c2f01479785c8bbd01b43
Paul Amoah <51966806+paglobal@users.noreply.github.com>
2025-06-30 11:11:08 +0000

    Updated orchestrator.ts

--- src/orchestrator.ts
+++ src/orchestrator.ts
@@ -8,19 +8,36 @@ import { drawEnemiesGraphics } from "./enemies";
 import { drawKeysGraphics } from "./keys";
 import { drawPowerUpsGraphics } from "./powerUps";
 import { Program } from "./interpreter";
+import { keybindingUtils, programUtils } from "./orchestratorUtils";
+import hotkeys from "hotkeys-js";
 
-const [orchestratorState, setOrchestratorState] = adaptState<{
-  level: number;
-  currentStatementsIndex: number;
-}>({ level: 1, currentStatementsIndex: 0 });
+export const [orchestratorState, setOrchestratorState] = adaptState({
+  level: 1,
+  currentStatementsIndex: 0,
+  playing: false,
+});
 
 const [program, setProgram] = adaptState<Program>([
-  { id: crypto.randomUUID(), type: "Statements", contents: [] },
+  programUtils.generateStatements(),
 ]);
 
 export const currentStatements = adaptMemo(() => {
   return program()[orchestratorState().currentStatementsIndex];
 });
+
+function initializeKeybindings() {
+  hotkeys("h,left", keybindingUtils.exitCurrentShard);
+  hotkeys("l,right", keybindingUtils.enterCurrentShard);
+  hotkeys("j,down", keybindingUtils.focusNextSiblingShard);
+  hotkeys("k,up", keybindingUtils.focusPreviousSiblingShard);
+  hotkeys("a", keybindingUtils.addShardInFrontOfCurrentShardAndFocus);
+  hotkeys("i", keybindingUtils.addShardBehindCurrentShardAndFocus);
+  hotkeys("d", keybindingUtils.deleteCurrentShard);
+  hotkeys("b", keybindingUtils.toggleCurrentShardDisplayState);
+  hotkeys("t", keybindingUtils.toggleCurrentShardType);
+  hotkeys("e", keybindingUtils.executeCurrentStatements);
+  hotkeys("space", keybindingUtils.toggleGameLoopPlayingState);
+}
 
 export async function initializeGame(canvas?: HTMLCanvasElement) {
   const app = new Application();
@@ -39,6 +56,8 @@ export async function initializeGame(canvas?: HTMLCanvasElement) {
   drawKeysGraphics(app.stage);
   drawEnemiesGraphics(app.stage);
   drawPlayerGraphics(app.stage);
+
+  initializeKeybindings();
 
   app.ticker.add((ticker) => {
     // update playerGraphics
